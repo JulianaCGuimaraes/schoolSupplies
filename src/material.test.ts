bb93193d8a818d6e24794b25237d95495a11cbb4
JulianaCGuimaraes <jujucg15@gmail.com>
2022-10-14 10:25:07 -0300

    test

---
+++ src/material.test.ts
@@ -0,0 +1,9 @@
+describe("material", () =>{
+    describe("get material", () => {
+        describe("given the product does not exist", () => {
+            it("Should return a 404", () => {
+                expect(true).toBe(true);
+            });
+        });
+    });
+});
